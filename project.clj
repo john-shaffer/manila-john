7797d43505e7e71ed91095fde42c8b20bd6d3053
Michael Shaffer <biocgefahr@gmail.com>
2014-11-03 11:42:18 -0600

    Add url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject manila-john "0.1.0-SNAPSHOT"
   :description "A CouchDB library which implements only what clutch does not."
-  :url ""
+  :url "https://github.com/dunklerfox/manila-john"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
